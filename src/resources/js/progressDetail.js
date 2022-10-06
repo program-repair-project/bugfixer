89a32f60926d3c874cd416a41ee0af0663986889
since1909 <since1909@daum.net>
2022-10-06 13:57:16 +0900

    Add : setProgressStatus() and printProgress()

--- src/resources/js/progressDetail.js
+++ src/resources/js/progressDetail.js
@@ -1,5 +1,35 @@
-/* JS will not correctly work when switching between desktop and mobile unless in debug mode */
+window.addEventListener('message', event => {
 
-$(function(){
-	
+    // The JSON data our extension sent
+    const message = event.data; 
+
+    var template = $.trim($('#template-log').html());
+    var container;
+
+    // 상태값에 따라 어떤 목록에 로그 추가할지 결정
+    switch (message.status) {
+        case 'Build':
+            container = $('#buildReadMore');
+            break;
+        case 'Analyze':
+            container = $('#analyzeReadMore');
+            break;
+        case 'Patch':
+            container = $('#patchReadMore');
+            break;
+        case 'Validate':
+            container = $('#validateReadMore');
+            break;
+    }
+
+    // template 으로 html 추가하기
+    var newLog = template;
+    for (var i in message) {
+        if(i == "status") continue;
+
+        var pattern = new RegExp('\\{\\{' + i + '\\}\\}', 'g')
+        newLog = newLog.replace(pattern, message[i])
+    }
+    container.append(newLog);
+    
 });
--- src/ui/webview_controller.ts
+++ src/ui/webview_controller.ts
@@ -9,43 +9,65 @@ import * as fs from 'fs';
 
 export class WebviewController {
     private _commandForProgressDetail: Disposable;
-    private _htmlSrc: String = "";
+
+    // Only allow a single progress view
+    private currentPanel: vscode.WebviewPanel | undefined = undefined;
+    private _htmlSrc: string = "";
+    
+    // 임시로 build 로 설정  (다른 값으로 변경하면 추가되는 열 바뀜)
+    private progressStatus: string = "Build";
   
     public constructor(private context: vscode.ExtensionContext) {
       this._commandForProgressDetail = commands.registerCommand("bugfixer.progress_detail",
         (uri: vscode.Uri) => {
           this.progress_detail(uri, context);
         });
+
+        // 메시지 전송 테스트를 위해 임시로 생성한 명령어
+        context.subscriptions.push(commands.registerCommand("print_progress_test", () => {
+            // 엔진 로그 찍을 때 이와 같은 형태로 사용
+            this.printProgress("TESTING PRINT LOG FUNCTION", "01:47 PM", "test test message message extension extension");
+        }));
     }
   
     public dispose() {
-      this._commandForProgressDetail.dispose();
+        this._commandForProgressDetail.dispose();
     }
 
     protected progress_detail(uri: vscode.Uri, context: vscode.ExtensionContext) {
-        // create and show panel
-        const panel = vscode.window.createWebviewPanel(
-            'progressDetail',
-            'Progress Detail',
-            vscode.ViewColumn.One,
-            {
-                // local Resource Path
-                localResourceRoots: [vscode.Uri.file(path.join(context.extensionPath, 'src/resources'))],
-                // enable scripts in the webview
-                enableScripts: true,
-                // for chrome DevTools
-                enableFindWidget: true
-            }
-        );
+        if (this.currentPanel) {
+            this.currentPanel.reveal(vscode.ViewColumn.One);
+        } else {
+            // create and show panel
+            this.currentPanel = vscode.window.createWebviewPanel(
+                'progressDetail',
+                'Progress Detail',
+                vscode.ViewColumn.One,
+                {
+                    // local Resource Path
+                    localResourceRoots: [vscode.Uri.file(path.join(context.extensionPath, 'src/resources'))],
+                    // enable scripts in the webview
+                    enableScripts: true,
+                    // for chrome DevTools
+                    enableFindWidget: true
+                }
+            );
 
-        const cssPath = path.join(context.extensionPath, 'src/resources', 'css');
-        const jsPath = path.join(context.extensionPath, 'src/resources', 'js');
+            const cssPath = path.join(context.extensionPath, 'src/resources', 'css');
+            const jsPath = path.join(context.extensionPath, 'src/resources', 'js');
 
-        const ProgressDetailCss = panel.webview.asWebviewUri(vscode.Uri.file(path.join(cssPath, 'progressDetail.css')));
-        const ProgressDetailJs = panel.webview.asWebviewUri(vscode.Uri.file(path.join(jsPath, 'progressDetail.js')));
+            const ProgressDetailCss = this.currentPanel.webview.asWebviewUri(vscode.Uri.file(path.join(cssPath, 'progressDetail.css')));
+            const ProgressDetailJs = this.currentPanel.webview.asWebviewUri(vscode.Uri.file(path.join(jsPath, 'progressDetail.js')));
 
-        // set panel html content
-        panel.webview.html = this.getProgressDetail(context, ProgressDetailCss, ProgressDetailJs);
+            // set panel html content
+            this.currentPanel.webview.html = this.getProgressDetail(context, ProgressDetailCss, ProgressDetailJs);
+
+            this.currentPanel.onDidDispose(
+                () => {
+                    this.currentPanel = undefined;
+                }
+            );
+        }
     }
 
     private getProgressDetail(context: vscode.ExtensionContext, webveiwCss: vscode.Uri, webviewJs: vscode.Uri) {
@@ -57,5 +79,25 @@ export class WebviewController {
         const contents = this._htmlSrc.replace(/WEBVIEWCSS/g, webveiwCss.toString()).replace(/WEBVIEWJS/g, webviewJs.toString());
 
         return contents;
+    }
+
+    public setProgressStatus(status: string) {
+        // 엔진 상태에 따라 상태값 변경 (Build, Analyze, Patch, Validate)
+        this.progressStatus = status;
+    }
+
+    private getProgressStatus() {
+        return this.progressStatus;
+    }
+
+    public printProgress(title: string, time: string, message: string) {
+        // html 로 메시지 전송
+        if (!this.currentPanel) {
+            return;
+        }
+
+        var jsonData = { status : this.getProgressStatus(), title: title, time : time, message : message}
+
+        this.currentPanel.webview.postMessage(jsonData);
     }
 }
